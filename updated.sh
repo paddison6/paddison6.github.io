4b731d9fff8064c894b3a0f1ccbb4c2a4deb1cf2
Mert Erdemir <merterdemir@gatech.edu>
2023-04-05 17:40:51 -0400

    news and last update date is changed

--- updated.sh
+++ updated.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DATE="2\ April\ 2023"
+DATE="5\ April\ 2023"
 find . -name "*.html" -type f -exec sed -i '' -e 's/^.*Last\ Update.*$/                    Last\ Update\:\ '"$DATE"'/g' {} \;
