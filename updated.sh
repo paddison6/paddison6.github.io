84217d011473c68b863cb8f5fa632a644aacefab
Mert Erdemir <merterdemir@gatech.edu>
2023-04-30 23:44:49 -0400

    30 april 2023

--- updated.sh
+++ updated.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-DATE="24\ April\ 2023"
+DATE="30\ April\ 2023"
 find . -name "*.html" -type f -exec sed -i '' -e 's/^.*Last\ Update.*$/                    Last\ Update\:\ '"$DATE"'/g' {} \;
